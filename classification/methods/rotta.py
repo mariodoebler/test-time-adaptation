d594f875dc16b3055bcc2246c1e683a51f4f4c37
RobMars <robert.a.marsden@hotmail.com>
2023-05-02 10:32:35 +0200

    Add new adaptation method RoTTA

---
+++ classification/methods/rotta.py
@@ -0,0 +1,333 @@
+"""
+Builds upon: https://github.com/BIT-DA/RoTTA
+Corresponding paper: https://arxiv.org/pdf/2303.13899.pdf
+"""
+
+import math
+import torch
+import torch.nn as nn
+
+from copy import deepcopy
+from methods.base import TTAMethod
+from augmentations.transforms_cotta import get_tta_transforms
+
+
+class RoTTA(TTAMethod):
+    def __init__(self, model, optimizer, steps, episodic, window_length, dataset_name, memory_size, num_classes, lambda_t, lambda_u, nu, update_freq):
+        super().__init__(model.cuda(), optimizer, steps, episodic, window_length)
+
+        self.memory_size = memory_size
+        self.num_classes = num_classes
+        self.lambda_t = lambda_t
+        self.lambda_u = lambda_u
+        self.nu = nu
+        self.update_frequency = update_freq  # actually the same as the size of memory bank
+        self.current_instance = 0
+        self.mem = CSTU(capacity=self.memory_size, num_class=self.num_classes, lambda_t=self.lambda_t, lambda_u=self.lambda_u)
+
+        # setup the ema model
+        self.model_ema = self.copy_model(self.model)
+        for param in self.model_ema.parameters():
+            param.detach_()
+
+        # note: if the self.model is never reset, like for continual adaptation,
+        # then skipping the state copy would save memory
+        self.models = [self.model, self.model_ema]
+        self.model_states, self.optimizer_state = self.copy_model_and_optimizer()
+
+        # create the test-time transformations
+        self.transform = get_tta_transforms(dataset_name)
+
+    @torch.enable_grad()
+    def forward_and_adapt(self, x):
+        imgs_test = x[0]
+
+        with torch.no_grad():
+            self.model.eval()
+            self.model_ema.eval()
+            ema_out = self.model_ema(imgs_test)
+            predict = torch.softmax(ema_out, dim=1)
+            pseudo_label = torch.argmax(predict, dim=1)
+            entropy = torch.sum(- predict * torch.log(predict + 1e-6), dim=1)
+
+        # add into memory
+        for i, data in enumerate(imgs_test):
+            p_l = pseudo_label[i].item()
+            uncertainty = entropy[i].item()
+            current_instance = (data, p_l, uncertainty)
+            self.mem.add_instance(current_instance)
+            self.current_instance += 1
+
+            if self.current_instance % self.update_frequency == 0:
+                self.update_model()
+
+        return ema_out
+
+    def update_model(self):
+        self.model.train()
+        self.model_ema.train()
+        # get memory data
+        sup_data, ages = self.mem.get_memory()
+        l_sup = None
+        if len(sup_data) > 0:
+            sup_data = torch.stack(sup_data)
+            strong_sup_aug = self.transform(sup_data)
+            ema_sup_out = self.model_ema(sup_data)
+            stu_sup_out = self.model(strong_sup_aug)
+            instance_weight = timeliness_reweighting(ages)
+            l_sup = (softmax_entropy(stu_sup_out, ema_sup_out) * instance_weight).mean()
+
+        l = l_sup
+        if l is not None:
+            self.optimizer.zero_grad()
+            l.backward()
+            self.optimizer.step()
+
+        self.update_ema_variables(self.model_ema, self.model, self.nu)
+
+    def reset(self):
+        if self.model_states is None or self.optimizer_state is None:
+            raise Exception("cannot reset without saved self.model/optimizer state")
+        self.load_model_and_optimizer()
+        self.current_instance = 0
+        self.mem = CSTU(capacity=self.memory_size,
+                        num_class=self.num_classes,
+                        lambda_t=self.lambda_t,
+                        lambda_u=self.lambda_u)
+
+    @staticmethod
+    def update_ema_variables(ema_model, model, nu):
+        for ema_param, param in zip(ema_model.parameters(), model.parameters()):
+            ema_param.data[:] = (1 - nu) * ema_param[:].data[:] + nu * param[:].data[:]
+        return ema_model
+
+    @staticmethod
+    def configure_model(model, alpha):
+        model.requires_grad_(False)
+        normlayer_names = []
+
+        for name, sub_module in model.named_modules():
+            if isinstance(sub_module, nn.BatchNorm1d) or isinstance(sub_module, nn.BatchNorm2d):
+                normlayer_names.append(name)
+
+        for name in normlayer_names:
+            bn_layer = get_named_submodule(model, name)
+            if isinstance(bn_layer, nn.BatchNorm1d):
+                NewBN = RobustBN1d
+            elif isinstance(bn_layer, nn.BatchNorm2d):
+                NewBN = RobustBN2d
+            else:
+                raise RuntimeError()
+
+            momentum_bn = NewBN(bn_layer, alpha)
+            momentum_bn.requires_grad_(True)
+            set_named_submodule(model, name, momentum_bn)
+        return model
+
+
+@torch.jit.script
+def softmax_entropy(x, x_ema):
+    return -(x_ema.softmax(1) * x.log_softmax(1)).sum(1)
+
+
+def timeliness_reweighting(ages):
+    if isinstance(ages, list):
+        ages = torch.tensor(ages).float().cuda()
+    return torch.exp(-ages) / (1 + torch.exp(-ages))
+
+
+def get_named_submodule(model, sub_name: str):
+    names = sub_name.split(".")
+    module = model
+    for name in names:
+        module = getattr(module, name)
+
+    return module
+
+
+def set_named_submodule(model, sub_name, value):
+    names = sub_name.split(".")
+    module = model
+    for i in range(len(names)):
+        if i != len(names) - 1:
+            module = getattr(module, names[i])
+
+        else:
+            setattr(module, names[i], value)
+
+
+class MomentumBN(nn.Module):
+    def __init__(self, bn_layer: nn.BatchNorm2d, momentum):
+        super().__init__()
+        self.num_features = bn_layer.num_features
+        self.momentum = momentum
+        if bn_layer.track_running_stats and bn_layer.running_var is not None and bn_layer.running_mean is not None:
+            self.register_buffer("source_mean", deepcopy(bn_layer.running_mean))
+            self.register_buffer("source_var", deepcopy(bn_layer.running_var))
+            self.source_num = bn_layer.num_batches_tracked
+        self.weight = deepcopy(bn_layer.weight)
+        self.bias = deepcopy(bn_layer.bias)
+
+        self.register_buffer("target_mean", torch.zeros_like(self.source_mean))
+        self.register_buffer("target_var", torch.ones_like(self.source_var))
+        self.eps = bn_layer.eps
+
+        self.current_mu = None
+        self.current_sigma = None
+
+    def forward(self, x):
+        raise NotImplementedError
+
+
+class RobustBN1d(MomentumBN):
+    def forward(self, x):
+        if self.training:
+            b_var, b_mean = torch.var_mean(x, dim=0, unbiased=False, keepdim=False)  # (C,)
+            mean = (1 - self.momentum) * self.source_mean + self.momentum * b_mean
+            var = (1 - self.momentum) * self.source_var + self.momentum * b_var
+            self.source_mean, self.source_var = deepcopy(mean.detach()), deepcopy(var.detach())
+            mean, var = mean.view(1, -1), var.view(1, -1)
+        else:
+            mean, var = self.source_mean.view(1, -1), self.source_var.view(1, -1)
+
+        x = (x - mean) / torch.sqrt(var + self.eps)
+        weight = self.weight.view(1, -1)
+        bias = self.bias.view(1, -1)
+
+        return x * weight + bias
+
+
+class RobustBN2d(MomentumBN):
+    def forward(self, x):
+        if self.training:
+            b_var, b_mean = torch.var_mean(x, dim=[0, 2, 3], unbiased=False, keepdim=False)  # (C,)
+            mean = (1 - self.momentum) * self.source_mean + self.momentum * b_mean
+            var = (1 - self.momentum) * self.source_var + self.momentum * b_var
+            self.source_mean, self.source_var = deepcopy(mean.detach()), deepcopy(var.detach())
+            mean, var = mean.view(1, -1, 1, 1), var.view(1, -1, 1, 1)
+        else:
+            mean, var = self.source_mean.view(1, -1, 1, 1), self.source_var.view(1, -1, 1, 1)
+
+        x = (x - mean) / torch.sqrt(var + self.eps)
+        weight = self.weight.view(1, -1, 1, 1)
+        bias = self.bias.view(1, -1, 1, 1)
+
+        return x * weight + bias
+
+
+class MemoryItem:
+    def __init__(self, data=None, uncertainty=0, age=0):
+        self.data = data
+        self.uncertainty = uncertainty
+        self.age = age
+
+    def increase_age(self):
+        if not self.empty():
+            self.age += 1
+
+    def get_data(self):
+        return self.data, self.uncertainty, self.age
+
+    def empty(self):
+        return self.data == "empty"
+
+
+class CSTU:
+    def __init__(self, capacity, num_class, lambda_t=1.0, lambda_u=1.0):
+        self.capacity = capacity
+        self.num_class = num_class
+        self.per_class = self.capacity / self.num_class
+        self.lambda_t = lambda_t
+        self.lambda_u = lambda_u
+
+        self.data: list[list[MemoryItem]] = [[] for _ in range(self.num_class)]
+
+    def get_occupancy(self):
+        occupancy = 0
+        for data_per_cls in self.data:
+            occupancy += len(data_per_cls)
+        return occupancy
+
+    def per_class_dist(self):
+        per_class_occupied = [0] * self.num_class
+        for cls, class_list in enumerate(self.data):
+            per_class_occupied[cls] = len(class_list)
+
+        return per_class_occupied
+
+    def add_instance(self, instance):
+        assert (len(instance) == 3)
+        x, prediction, uncertainty = instance
+        new_item = MemoryItem(data=x, uncertainty=uncertainty, age=0)
+        new_score = self.heuristic_score(0, uncertainty)
+        if self.remove_instance(prediction, new_score):
+            self.data[prediction].append(new_item)
+        self.add_age()
+
+    def remove_instance(self, cls, score):
+        class_list = self.data[cls]
+        class_occupied = len(class_list)
+        all_occupancy = self.get_occupancy()
+        if class_occupied < self.per_class:
+            if all_occupancy < self.capacity:
+                return True
+            else:
+                majority_classes = self.get_majority_classes()
+                return self.remove_from_classes(majority_classes, score)
+        else:
+            return self.remove_from_classes([cls], score)
+
+    def remove_from_classes(self, classes: list[int], score_base):
+        max_class = None
+        max_index = None
+        max_score = None
+        for cls in classes:
+            for idx, item in enumerate(self.data[cls]):
+                uncertainty = item.uncertainty
+                age = item.age
+                score = self.heuristic_score(age=age, uncertainty=uncertainty)
+                if max_score is None or score >= max_score:
+                    max_score = score
+                    max_index = idx
+                    max_class = cls
+
+        if max_class is not None:
+            if max_score > score_base:
+                self.data[max_class].pop(max_index)
+                return True
+            else:
+                return False
+        else:
+            return True
+
+    def get_majority_classes(self):
+        per_class_dist = self.per_class_dist()
+        max_occupied = max(per_class_dist)
+        classes = []
+        for i, occupied in enumerate(per_class_dist):
+            if occupied == max_occupied:
+                classes.append(i)
+
+        return classes
+
+    def heuristic_score(self, age, uncertainty):
+        return self.lambda_t * 1 / (1 + math.exp(-age / self.capacity)) + self.lambda_u * uncertainty / math.log(self.num_class)
+
+    def add_age(self):
+        for class_list in self.data:
+            for item in class_list:
+                item.increase_age()
+        return
+
+    def get_memory(self):
+        tmp_data = []
+        tmp_age = []
+
+        for class_list in self.data:
+            for item in class_list:
+                tmp_data.append(item.data)
+                tmp_age.append(item.age)
+
+        tmp_age = [x / self.capacity for x in tmp_age]
+
+        return tmp_data, tmp_age

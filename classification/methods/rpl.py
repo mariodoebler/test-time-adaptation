f1055fad70d5db8564cec8492efc7400c8087cc3
RobMars <robert.a.marsden@hotmail.com>
2023-12-21 17:37:26 +0100

    Add new method RPL

---
+++ classification/methods/rpl.py
@@ -0,0 +1,68 @@
+"""
+Builds upon: https://github.com/oripress/CCC/blob/main/models/rpl.py
+Corresponding paper: https://arxiv.org/pdf/2104.12928.pdf
+"""
+
+import torch
+import torch.nn as nn
+
+from methods.base import TTAMethod
+from utils.registry import ADAPTATION_REGISTRY
+from utils.losses import GeneralizedCrossEntropy
+
+
+@ADAPTATION_REGISTRY.register()
+class RPL(TTAMethod):
+    def __init__(self, cfg, model, num_classes):
+        super().__init__(cfg, model, num_classes)
+        self.gce = GeneralizedCrossEntropy(q=self.cfg.RPL.Q)
+
+    @torch.enable_grad()  # ensure grads in possible no grad context for testing
+    def forward_and_adapt(self, x):
+        imgs_test = x[0]
+        outputs = self.model(imgs_test)
+        labels = outputs.argmax(dim=1)
+        loss = self.gce(outputs, targets=labels).mean(0)
+        loss.backward()
+        self.optimizer.step()
+        self.optimizer.zero_grad(set_to_none=True)
+        return outputs
+
+    def collect_params(self):
+        """Collect the affine scale + shift parameters from batch norms.
+
+        Walk the model's modules and collect all batch normalization parameters.
+        Return the parameters and their names.
+
+        Note: other choices of parameterization are possible!
+        """
+        params = []
+        names = []
+        for nm, m in self.model.named_modules():
+            if isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d, nn.LayerNorm, nn.GroupNorm)):
+                for np, p in m.named_parameters():
+                    if np in ['weight', 'bias']:  # weight is scale, bias is shift
+                        params.append(p)
+                        names.append(f"{nm}.{np}")
+        return params, names
+
+    def configure_model(self):
+        """Configure model for use with tent."""
+        # train mode, because tent optimizes the model to minimize entropy
+        # self.model.train()
+        self.model.eval()  # eval mode to avoid stochastic depth in swin. test-time normalization is still applied
+        # disable grad, to (re-)enable only what tent updates
+        self.model.requires_grad_(False)
+        # configure norm for tent updates: enable grad + force batch statisics
+        for m in self.model.modules():
+            if isinstance(m, nn.BatchNorm2d):
+                m.requires_grad_(True)
+                # force use of batch stats in train and eval modes
+                m.track_running_stats = False
+                m.running_mean = None
+                m.running_var = None
+            elif isinstance(m, nn.BatchNorm1d):
+                m.train()   # always forcing train mode in bn1d will cause problems for single sample tta
+                m.requires_grad_(True)
+            elif isinstance(m, (nn.LayerNorm, nn.GroupNorm)):
+                m.requires_grad_(True)

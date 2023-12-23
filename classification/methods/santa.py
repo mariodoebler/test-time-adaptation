c10d230848b3cb3872520fce59af7b8df64da898
Goirik <chakrabarty.goirik@gmail.com>
2023-12-23 12:35:03 +0530

    Added SANTA and configs

---
+++ classification/methods/santa.py
@@ -0,0 +1,229 @@
+import logging
+
+import os
+import tqdm
+import torch
+import torch.nn as nn
+import torch.jit
+import torch.nn.functional as F
+
+from methods.base import TTAMethod
+from models.model import split_up_model
+from augmentations.transforms_cotta import get_tta_transforms
+from datasets.data_loading import get_source_loader
+from utils.registry import ADAPTATION_REGISTRY
+from utils.losses import SymmetricCrossEntropy, AugCrossEntropy
+
+logger = logging.getLogger(__name__)
+
+@ADAPTATION_REGISTRY.register()
+class SANTA(TTAMethod):
+    def __init__(self, cfg, model, num_classes):
+        super().__init__(cfg, model, num_classes)
+
+        batch_size_src = cfg.TEST.BATCH_SIZE if cfg.TEST.BATCH_SIZE > 1 else cfg.TEST.WINDOW_LENGTH
+        _, self.src_loader = get_source_loader(dataset_name=cfg.CORRUPTION.DATASET,
+                                               adaptation=cfg.MODEL.ADAPTATION,
+                                               preprocess=model.model_preprocess,
+                                               data_root_dir=cfg.DATA_DIR,
+                                               batch_size=batch_size_src,
+                                               ckpt_path=cfg.MODEL.CKPT_PATH,
+                                               num_samples=cfg.SOURCE.NUM_SAMPLES,
+                                               percentage=cfg.SOURCE.PERCENTAGE,
+                                               workers=min(cfg.SOURCE.NUM_WORKERS, os.cpu_count()))
+        self.contrast_mode = cfg.CONTRAST.MODE
+        self.temperature = cfg.CONTRAST.TEMPERATURE
+        self.base_temperature = self.temperature
+        self.projection_dim = cfg.CONTRAST.PROJECTION_DIM
+        self.lambda_ce_trg = cfg.SANTA.LAMBDA_CE_TRG
+        self.lambda_cont = cfg.SANTA.LAMBDA_CONT
+        self.final_lr = cfg.OPTIM.LR
+        arch_name = cfg.MODEL.ARCH
+        ckpt_path = cfg.MODEL.CKPT_PATH
+
+        self.tta_transform = get_tta_transforms(self.dataset_name)
+
+        # setup loss functions
+        # self.symmetric_cross_entropy = SymmetricCrossEntropy()
+        self.aug_entropy = AugCrossEntropy()
+
+        # Setup Anchor model
+        self.anchor_model = self.copy_model(self.model)
+        for param in self.anchor_model.parameters():
+            param.detach_()
+
+        # split up the model
+        self.feature_extractor, self.classifier = split_up_model(self.model, arch_name, self.dataset_name)
+
+        # define the prototype paths
+        proto_dir_path = os.path.join(cfg.CKPT_DIR, "prototypes")
+        if self.dataset_name == "domainnet126":
+            fname = f"protos_{self.dataset_name}_{ckpt_path.split(os.sep)[-1].split('_')[1]}.pth"
+        else:
+            fname = f"protos_{self.dataset_name}_{arch_name}.pth"
+        fname = os.path.join(proto_dir_path, fname)
+
+        # get source prototypes
+        if os.path.exists(fname):
+            logger.info("Loading class-wise source prototypes...")
+            self.prototypes_src = torch.load(fname)
+        else:
+            os.makedirs(proto_dir_path, exist_ok=True)
+            features_src = torch.tensor([])
+            labels_src = torch.tensor([])
+            logger.info("Extracting source prototypes...")
+            with torch.no_grad():
+                for data in tqdm.tqdm(self.src_loader):
+                    x, y = data[0], data[1]
+                    tmp_features = self.feature_extractor(x.to(self.device))
+                    features_src = torch.cat([features_src, tmp_features.view(tmp_features.shape[:2]).cpu()], dim=0)
+                    labels_src = torch.cat([labels_src, y], dim=0)
+                    if len(features_src) > 100000:
+                        break
+
+            # create class-wise source prototypes
+            self.prototypes_src = torch.tensor([])
+            for i in range(self.num_classes):
+                mask = labels_src == i
+                self.prototypes_src = torch.cat([self.prototypes_src, features_src[mask].mean(dim=0, keepdim=True)], dim=0)
+
+            torch.save(self.prototypes_src, fname)
+
+        self.prototypes_src = self.prototypes_src.to(self.device).unsqueeze(1)
+        self.prototype_labels_src = torch.arange(start=0, end=self.num_classes, step=1).to(self.device).long()
+
+        # setup projector
+        if self.dataset_name == "domainnet126":
+            # do not use a projector since the network already clusters the features and reduces the dimensions
+            self.projector = nn.Identity()
+        else:
+            num_channels = self.prototypes_src.shape[-1]
+            self.projector = nn.Sequential(nn.Linear(num_channels, self.projection_dim), nn.ReLU(),
+                                           nn.Linear(self.projection_dim, self.projection_dim)).to(self.device)
+            self.optimizer.add_param_group({'params': self.projector.parameters(), 'lr': self.optimizer.param_groups[0]["lr"]})
+
+        # note: if the self.model is never reset, like for continual adaptation,
+        # then skipping the state copy would save memory
+        self.models = [self.model, self.anchor_model, self.projector]
+        self.model_states, self.optimizer_state = self.copy_model_and_optimizer()
+
+    # Integrated from: https://github.com/HobbitLong/SupContrast/blob/master/losses.py
+    def contrastive_loss(self, features, labels=None, mask=None):
+        batch_size = features.shape[0]
+        if labels is not None and mask is not None:
+            raise ValueError('Cannot define both `labels` and `mask`')
+        elif labels is None and mask is None:
+            mask = torch.eye(batch_size, dtype=torch.float32).to(self.device)
+        elif labels is not None:
+            labels = labels.contiguous().view(-1, 1)
+            if labels.shape[0] != batch_size:
+                raise ValueError('Num of labels does not match num of features')
+            mask = torch.eq(labels, labels.T).float().to(self.device)
+        else:
+            mask = mask.float().to(self.device)
+
+        contrast_count = features.shape[1]
+        contrast_feature = torch.cat(torch.unbind(features, dim=1), dim=0)
+        contrast_feature = self.projector(contrast_feature)
+        contrast_feature = F.normalize(contrast_feature, p=2, dim=1)
+        if self.contrast_mode == 'one':
+            anchor_feature = features[:, 0]
+            anchor_count = 1
+        elif self.contrast_mode == 'all':
+            anchor_feature = contrast_feature
+            anchor_count = contrast_count
+        else:
+            raise ValueError('Unknown mode: {}'.format(self.contrast_mode))
+
+        # compute logits
+        anchor_dot_contrast = torch.div(torch.matmul(anchor_feature, contrast_feature.T), self.temperature)
+        # for numerical stability
+        logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
+        logits = anchor_dot_contrast - logits_max.detach()
+
+        # tile mask
+        mask = mask.repeat(anchor_count, contrast_count)
+        # mask-out self-contrast cases
+        logits_mask = torch.scatter(
+            torch.ones_like(mask),
+            1,
+            torch.arange(batch_size * anchor_count).view(-1, 1).to(self.device),
+            0
+        )
+        mask = mask * logits_mask
+
+        # compute log_prob
+        exp_logits = torch.exp(logits) * logits_mask
+        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+
+        # compute mean of log-likelihood over positive
+        mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
+
+        # loss
+        loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
+        loss = loss.view(anchor_count, batch_size).mean()
+        return loss
+
+    @torch.enable_grad()  # ensure grads in possible no grad context for testing
+    def forward_and_adapt(self, x):
+        imgs_test = x[0]
+
+        self.optimizer.zero_grad()
+
+        # forward original test data
+        features_test = self.feature_extractor(imgs_test)
+        outputs_test = self.classifier(features_test)
+
+        # forward augmented test data
+        features_aug_test = self.feature_extractor(self.tta_transform((imgs_test)))
+        outputs_aug_test = self.classifier(features_aug_test)
+
+        # forward original test data through the anchor model
+        outputs_anchor = self.anchor_model(imgs_test)
+
+        with torch.no_grad():
+            # dist[:, i] contains the distance from every source sample to one test sample
+            dist = F.cosine_similarity(
+                x1=self.prototypes_src.repeat(1, features_test.shape[0], 1),
+                x2=features_test.view(1, features_test.shape[0], features_test.shape[1]).repeat(self.prototypes_src.shape[0], 1, 1),
+                dim=-1)
+
+            # for every test feature, get the nearest source prototype and derive the label
+            _, indices = dist.topk(1, largest=True, dim=0)
+            indices = indices.squeeze(0)
+
+        features = torch.cat([self.prototypes_src[indices],
+                              features_test.view(features_test.shape[0], 1, features_test.shape[1]),
+                              features_aug_test.view(features_test.shape[0], 1, features_test.shape[1])], dim=1)
+        loss_contrastive = self.contrastive_loss(features=features, labels=None)
+
+        # loss_self_training = (0.5 * self.symmetric_cross_entropy(outputs_test, outputs_anchor) + 0.5 * self.symmetric_cross_entropy(outputs_aug_test, outputs_anchor)).mean(0)
+        loss_self_training = self.aug_entropy(outputs_test, outputs_aug_test, outputs_anchor).mean(0)
+        loss_trg = self.lambda_ce_trg * loss_self_training + self.lambda_cont * loss_contrastive
+        loss_trg.backward()
+
+        self.optimizer.step()
+
+        # create and return the ensemble prediction
+        return outputs_test
+
+    def configure_model(self):
+        """Configure model for use with tent."""
+        # train mode, because tent optimizes the model to minimize entropy
+        # self.model.train()
+        self.model.eval()  # eval mode to avoid stochastic depth in swin. test-time normalization is still applied
+        # disable grad, to (re-)enable only what tent updates
+        self.model.requires_grad_(False)
+        # configure norm for tent updates: enable grad + force batch statisics
+        for m in self.model.modules():
+            if isinstance(m, nn.BatchNorm2d):
+                m.requires_grad_(True)
+                # force use of batch stats in train and eval modes
+                m.track_running_stats = False
+                m.running_mean = None
+                m.running_var = None
+            elif isinstance(m, nn.BatchNorm1d):
+                m.train()   # always forcing train mode in bn1d will cause problems for single sample tta
+                m.requires_grad_(True)
+            elif isinstance(m, (nn.LayerNorm, nn.GroupNorm)):
+                m.requires_grad_(True)

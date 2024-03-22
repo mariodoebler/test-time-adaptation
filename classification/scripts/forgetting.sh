ace15631c3f9db3026f45218ab35a75b3a65221c
Mario Döbler <m137@login1.lss.uni-stuttgart.de>
2024-03-22 22:01:33 +0100

    Reformating

--- classification/scripts/forgetting.sh
+++ classification/scripts/forgetting.sh
@@ -12,11 +12,11 @@ architectures=(resnet50 swin_b vit_b_16)
 for arch in ${architectures[*]}; do
   for mthd in ${methods[*]}; do
     for seed in ${seeds[*]}; do
-      python test_time.py --cfg "cfgs/imagenet_c/${mthd}.yaml" MODEL.ARCH $arch RNG_SEED $seed CORRUPTION.NUM_EX 50000 $options CORRUPTION.TYPE "['gaussian_noise', 'none',
-       'shot_noise', 'none', 'impulse_noise', 'none', 'defocus_blur', 'none', 'glass_blur', 'none',
-       'motion_blur', 'none', 'zoom_blur', 'none', 'snow', 'none', 'frost', 'none', 'fog', 'none',
-       'brightness', 'none', 'contrast', 'none', 'elastic_transform', 'none', 'pixelate', 'none',
-       'jpeg_compression', 'none']"
+      python test_time.py --cfg "cfgs/imagenet_c/${mthd}.yaml" MODEL.ARCH $arch RNG_SEED $seed CORRUPTION.NUM_EX 50000 $options CORRUPTION.TYPE "[
+        'gaussian_noise', 'none', 'shot_noise', 'none', 'impulse_noise', 'none',
+        'defocus_blur', 'none', 'glass_blur', 'none', 'motion_blur', 'none', 'zoom_blur', 'none',
+        'snow', 'none', 'frost', 'none', 'fog', 'none', 'brightness', 'none', 'contrast', 'none',
+        'elastic_transform', 'none', 'pixelate', 'none', 'jpeg_compression', 'none']"
     done
   done
 done
